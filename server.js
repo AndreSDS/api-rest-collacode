272ebf030617111b366ea9032f79655e2c73a8b6
AndréSDS <andre_smiths@outlook.com>
2018-11-17 18:43:24 -0200

    atualizando rota para get na const collabs

--- server.js
+++ server.js
@@ -3,16 +3,29 @@ const port = 8081;
 
 const server = express();
 
-server.get('/', (request, response) => {
-    response.send("<h1>Home</h1>");
+//verbos || method => get post put e delete
+
+const COLLABS = [
+    {
+    nick: 'andre',
+    qtd: 302
+},
+    {
+    nick: 'eduardo',
+    qtd:  345, 
+},
+    {
+    nick: 'bernardo',
+    qtd: 250
+}
+];
+
+server.get("/", (req, res) => {
+    res.send("<h1>Home</h1>");
 });
 
-server.get('/fotos', (request, response) => {
-    response.send("<h1>Fotos</h1> <img src='https://www.petz.com.br/blog/wp-content/uploads/2017/07/gato02.jpg'/>");
-});
-
-server.get('/contatos', (request, response) => {
-    response.send("<h1>Contatos</h1>");
+server.get("/collabs", (req, res) => {
+    res.send(COLLABS);
 });
 
 server.listen(port, () => {
